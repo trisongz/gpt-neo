9d1d4f77c142bb73009e9368d5511c1efef1d594
Stella Biderman <stellabiderman@gmail.com>
2020-12-05 22:57:30 -0500

    Update inputs.py

--- inputs.py
+++ inputs.py
@@ -44,7 +44,7 @@ def generic_text(params, eval=False, sample_text_fn=None, step=0):
 def text_dataset(files, params, stitch, datatype, batch=True, sample_text_fn=None):
     
     file_list = tf.data.Dataset.from_tensor_slices(files)
-    dataset = file_list.interleave(tf.data.TFRecordDataset, cycle_length=4))
+    dataset = file_list.interleave(tf.data.TFRecordDataset, cycle_length=4)
 
     def _parse(example_proto):
         doc = tf.parse_single_example(example_proto, {"text": tf.VarLenFeature(tf.int64)})["text"]
